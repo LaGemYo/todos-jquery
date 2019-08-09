7bbe5b7f648b0923232e570b18d2f9dd287db0b8
LaGemYo <lagemyo@gmail.com>
2019-08-09 23:51:35 +0200

    added delete todos functionality

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ import './index.css';
 //Check off specific to-do's by clicking
 $(document).ready(() => {
 
-  $("li").on('click', function () {
+  $("li").on('click', function() {
     $(this).toggleClass('completed')
   });
+
+//Click on X to delete to-document
+$("span").on('click', function(e) {
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  e.stopPropagation();
 });
 
 
@@ -18,10 +25,7 @@ $(document).ready(() => {
 
 
 
-
-
-
-
+});
 
 
 
