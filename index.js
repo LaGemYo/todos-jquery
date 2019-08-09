6d97fd7fe94498f6cdaea44ba530243484636b21
LaGemYo <lagemyo@gmail.com>
2019-08-09 13:54:28 +0200

    starting script

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
+import $ from "jquery";
+
 // Import stylesheets
 import './index.css';
 
-// Write Javascript code!
+//Check off specific to-do's by clicking
+$(document).ready(() => {
+
+  $("li").on('click', function() {
+    if($(this).css("color") === "rgb(128, 128, 128)"){
+      $(this).css({
+        color: "black",
+        textDecoration: "none",
+      });
+      } else {
+      $(this).css({
+        color: "gray",
+        textDecoration: "line-through",
+      })
+      }
+  })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
