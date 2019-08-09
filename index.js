c06d2da6b23573f89fb3331b5f46810b68a4ca1b
LaGemYo <lagemyo@gmail.com>
2019-08-10 00:15:13 +0200

    added delete todos functionality

--- index.js
+++ index.js
@@ -3,21 +3,31 @@ import $ from "jquery";
 // Import stylesheets
 import './index.css';
 
-//Check off specific to-do's by clicking
-$(document).ready(() => {
 
-  $("li").on('click', function() {
+$(document).ready(() => {
+  //Check off specific todos by clicking
+  $("ul").on('click', "li", function () {
     $(this).toggleClass('completed')
   });
 
-//Click on X to delete to-document
-$("span").on('click', function(e) {
-  $(this).parent().fadeOut(500, function(){
-    $(this).remove();
+  //Click on X to delete todos
+  $("span").on('click', "li", function (e) {
+    $(this).parent().fadeOut(500, function () {
+      $(this).remove();
+    });
+    e.stopPropagation();
   });
-  e.stopPropagation();
-});
 
+  //Add new todos
+  $("input[type='text']").on("keypress", function (e) {
+    if (e.which === 13) {
+      var todoText = $(this).val();
+      $(this).val("");
+      $('ul').append("<li>X<span> " + todoText + " </span></li>")
+    }
+
+
+  });
 
 
 
